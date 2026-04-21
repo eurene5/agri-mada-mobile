8d968ce839f4de2f76a8c561509975f558964dc1
Dilan LOCK <84476039+eurene5@users.noreply.github.com>
2026-04-21 23:56:54 +0300

    feat(theme): add color, spacing, typography tokens and app theme

---
+++ lib/app/theme/app_typography.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+abstract final class AppTypography {
+  static const String _fontFamily = 'Poppins';
+
+  static const TextStyle displayLarge = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 34,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static const TextStyle displayMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 25,
+    fontWeight: FontWeight.w900,
+    color: AppColors.primary,
+  );
+
+  static const TextStyle headlineLarge = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: AppColors.textPrimary,
+  );
+
+  static const TextStyle headlineMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static const TextStyle titleLarge = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 34,
+    fontWeight: FontWeight.w700,
+    color: AppColors.primary,
+  );
+
+  static const TextStyle titleMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 22,
+    fontWeight: FontWeight.w700,
+    color: AppColors.textPrimary,
+  );
+
+  static const TextStyle bodyLarge = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textPrimary,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textPrimary,
+  );
+
+  static const TextStyle bodySmall = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    color: AppColors.textSecondary,
+  );
+
+  static const TextStyle labelLarge = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: AppColors.textOnPrimary,
+  );
+
+  static const TextStyle labelMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: AppColors.textOnPrimary,
+  );
+
+  static const TextStyle labelSmall = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textSecondary,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textSecondary,
+  );
+}
